e170faf04910204e98a11186af2574e7a7137b07
Wellington <Wellington@GUGU-NOTE>
2021-09-16 23:26:34 -0300

    build template

---
+++ doc/tool/generate/build/Neg/GeraClasse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gcpbuild.Neg
+{
+    public class GeraClasse
+    {
+        private string _pastaLocal = "";
+        private string _pastaTemplates = "";
+        public string _pastaResultado = @"\Result\";
+        public GeraClasse()
+        {
+            _pastaLocal = Directory.GetCurrentDirectory();
+        }
+
+        public void ReplaceTemplates(string nomeChave)
+        {
+            var lstFiles = Directory.GetFiles(_pastaLocal + _pastaTemplates);
+
+
+            foreach (var filePath in lstFiles)
+            {
+                var file = new FileInfo(filePath);
+                var fileNameResultado = file.Name.Replace("NomeModel", nomeChave);
+                fileNameResultado = fileNameResultado.Replace(".txt", "");
+
+                StringBuilder sb = new StringBuilder(File.ReadAllText(filePath));
+                sb = sb.Replace("#nomemodel#", nomeChave.ToLower());
+                sb = sb.Replace("#NOMEMODEL#", nomeChave.ToUpper());
+                sb = sb.Replace("#NomeModel#", nomeChave);
+                if (!Directory.Exists(_pastaLocal + _pastaTemplates + _pastaResultado))
+                {
+                    Directory.CreateDirectory(_pastaLocal + _pastaTemplates + _pastaResultado);
+                }
+                File.WriteAllText(_pastaLocal + _pastaTemplates + _pastaResultado + fileNameResultado, sb.ToString());
+            }
+        }
+    }
+}
---
+++ doc/tool/generate/build/Program.cs
@@ -0,0 +1,41 @@
+﻿using gcpbuild.Neg;
+using System;
+
+namespace gcpbuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidationArgs(args);
+
+            string NomeEntidade = args[0];
+
+            var cls = new GeraClasse();
+            cls.ReplaceTemplates(NomeEntidade);
+            Console.WriteLine($"Concluído, arquivo gerado na pasta '{cls._pastaResultado}'");
+
+        }
+
+        private static void ValidationArgs(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Operação inválida: 1 argumento é necessário para aplicar a #NomeModel# nos arquivos do template");
+                Console.WriteLine("FECHAR");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+
+            if(args[0] == "-dir")
+            {
+                var dirLocal = System.IO.Directory.GetCurrentDirectory();
+                Console.WriteLine($"Pasta local: ´{dirLocal}´");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+        }
+
+        
+    }
+}
