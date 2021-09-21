b2edda103f66617984e55a6b3e34b8e327cfb44a
Wellington <Wellington@GUGU-NOTE>
2021-09-21 17:32:34 -0300

    ok

--- src/Data/Contexto/AppDbContext.cs
+++ src/Data/Contexto/AppDbContext.cs
@@ -23,21 +23,31 @@ namespace Data.Contexto
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //aplica padrão para entidades nao mapeadas
             foreach (var property in modelBuilder.Model.GetEntityTypes()
                 .SelectMany(e => e.GetProperties()))
             {
-                if(property.ClrType == typeof(string))
-                property.SetColumnType("varchar(1)");
-                
-                if(property.ClrType == typeof(decimal) || property.ClrType == typeof(decimal?))
-                property.SetColumnType("decimal(18,2)");
+                if (property.ClrType == typeof(string))
+                    property.SetColumnType("varchar(1)");
+
+                if (property.ClrType == typeof(decimal) || property.ClrType == typeof(decimal?))
+                    property.SetColumnType("decimal(18,2)");
             }
 
             // mappeia todas classes mappings de uma vez só
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
 
+            //apos mapeamento, substitui as mapeadas nvarchar para varchar
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetProperties()))
+            {
+                if (property.ClrType == typeof(string))
+                    property.SetIsUnicode(false);
+
+            }
+
             //Impedindo exclusão em cascata
-            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e=> e.GetForeignKeys()))
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
             {
                 relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
             }
