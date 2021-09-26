f15aebd8bdc8710e7fa36ce8a9f9ab965d3900b6
Wellington <Wellington@GUGU-NOTE>
2021-09-26 10:25:27 -0300

    app status

---
+++ src/Api-Application/Controllers/AppStatusController.cs
@@ -0,0 +1,45 @@
+﻿using ApiApplication.Extensions;
+using ApiApplication.ViewModel;
+using AutoMapper;
+using Business.Interface;
+using Business.Interface.Repository;
+using Business.Interface.Services;
+using Business.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ApiApplication.Controllers
+{
+     
+    [Route("api/status")]
+    public class AppStatusController 
+    {
+
+        const string Permissao = "STATUS";
+
+        private readonly ILogger<CadastroController> _logger;
+        private readonly IHostEnvironment _hostingEnvironment;
+
+        public AppStatusController(ILogger<CadastroController> logger,
+                                    IHostEnvironment hostingEnvironment)
+        {
+            _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet] 
+        public ActionResult<string> Get()
+        {
+            var STATUS = $"APP - ON as {_hostingEnvironment.EnvironmentName}";
+
+            return STATUS;            
+        } 
+    }
+}
