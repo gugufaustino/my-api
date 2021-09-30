a52befe78aef91bfdcf25256795f86dbae184be0
Wellington <Wellington@GUGU-NOTE>
2021-09-30 18:12:50 -0300

    .

--- BusinessTest/UnitTest1.cs
+++ BusinessTest/UnitTest1.cs
@@ -3,13 +3,13 @@ using Business.Notifications;
 using Business.Services;
 using Data.Repository;
 
+
 namespace BusinessTest
 {
     //[TestClass]
     public class UnitTest1
     {
-
-         
+        //string testHelloWorld = Class1.HelloWorld();         
 
     }
 }
